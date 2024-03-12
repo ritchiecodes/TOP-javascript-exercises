6dfca3238aae384ef6a54c9150a43ca0c2947184
Ritchie Caruso <ritchiecaruso7@gmail.com>
2024-03-12 20:41:10 +1100

    completed exercise 03 and 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+if (Math.sign(num) === -1) {
+    return "ERROR";
+} else {
+return string.repeat(num);
+}
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// console.log(repeatString("hello", -1));
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,42 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    const newArray = [];
+    array.forEach((item) => {
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
 
-};
+
+
+
+
+
+// const removeFromArray = function(array, ...valuesToRemove) {
+//     for (let i = 0; i < valuesToRemove.length; i++) {
+//         const index = array.indexOf(valuesToRemove[i]);
+        
+//         if (index !== -1) {
+//             array.splice(index, 1);
+//         }
+//     }
+//     return array;
+// }   
+    
+    
+    
+//     let indexForRemoval = array.indexOf(valuesToRemove);
+//     if (indexForRemoval === -1) {
+//         return;
+//     } else {
+//     array.splice(indexForRemoval, 1);
+//     return array;
+//     }
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// console.log(removeFromArray(["hello",2,3,4], "hello"));
+console.log(removeFromArray([1,2,2,3],2));
