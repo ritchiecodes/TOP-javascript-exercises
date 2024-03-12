985f4b31bbf472b00ca48806a4f1c8e3d0e7126b
Ritchie Caruso <ritchiecaruso7@gmail.com>
2024-03-12 21:36:20 +1100

    completed exercises 5, 6 and 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+if (min < 0 || max < 0) return "ERROR";
+if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
 
+sum = 0;
+for (let i = min; i <= max; i++) {
+    sum += i;   
+}
+return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+console.log(sumAll(1,5))
